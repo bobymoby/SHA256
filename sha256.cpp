577422e6f1a6f8c7e1baf445578e459a4071c8ba
Borimir Georgiev <imbobimobi@gmail.com>
2022-12-06 13:45:08 +0200

    comments and gitignoire

--- sha256.cpp
+++ sha256.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// First 32 bits of the fractional parts of the cube roots of the first 64 primes 2..311
 unsigned int K[64] = {
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -49,25 +50,29 @@ unsigned int Majority(unsigned int hash1, unsigned int hash2, unsigned int hash3
     return (hash1 & hash2) ^ (hash1 & hash3) ^ (hash2 & hash3);
 }
 
+//The SHA256 algorithm requires the message to be padded to a multiple of 512 bits(64 bytes)
+//The padding is done by adding a 1 bit, followed by 0 bits, followed by the length of the message in big endian format(8 full bytes)
 void Pad(string& message) {
     unsigned long long length = message.length() * 8;
-    message += (char)0x80;
+    message += (uint8_t)128; //10000000 in binary
     while (message.length() % 64 != 56) {
-        message += (char)0x00;
+        message += (char)0; //00000000 in binary
     }
     string bigEndianLength = "";
     for (int i = 0; i < 8; i++) {
-        bigEndianLength += (char)((length >> (8 * (7 - i))) & 0xFF);
+        bigEndianLength += (uint8_t)(length >> (8 * (7 - i)));
     }
     message += bigEndianLength;
 }
 
 void Transform(unsigned int Words[64], unsigned int Hash[8])
 {
+    //Transform the message W[0..15] into 64 words using the SHA256 algorithm
     for (int index = 16; index < 64; index++)
     {
         Words[index] = Words[index - 16] + Sigma0(Words[index - 15]) + Words[index - 7] + Sigma1(Words[index - 2]);
     }
+    //Working variables
     unsigned int a = Hash[0];
     unsigned int b = Hash[1];
     unsigned int c = Hash[2];
@@ -77,6 +82,7 @@ void Transform(unsigned int Words[64], unsigned int Hash[8])
     unsigned int g = Hash[6];
     unsigned int h = Hash[7];
 
+    //Compression function main loop
     for (int i = 0; i < 64; i++)
     {
         unsigned int temp1 = h + CapitalSigma1(e) + Choice(e, f, g) + K[i] + Words[i];
@@ -91,6 +97,7 @@ void Transform(unsigned int Words[64], unsigned int Hash[8])
         a = temp1 + temp2;
     }
 
+    //Update the hash values with the compressed chunk
     Hash[0] += a;
     Hash[1] += b;
     Hash[2] += c;
@@ -104,14 +111,18 @@ void Transform(unsigned int Words[64], unsigned int Hash[8])
 
 void Update(string& message, unsigned int Hash[8])
 {
-    for (int iterations = 0; iterations < message.size() / 64; iterations++)
+    for (int chunk = 0; chunk < message.size() / 64; chunk++)
     {
         unsigned int Words[64];
         for (int index = 0; index < 16; index++)
         {
             Words[index] = 0;
+            //Words[index] will become |aaaaaaaabbbbbbbbccccccccdddddddd|
+            //Where a,b,c,d are the bytes of chunk[index], chunk[index + 1], chunk[index + 2], chunk[index + 3]
+            //Where chunk is the current 64 byte chunk of the message
             for (int j = 0; j < 4; j++) {
-                Words[index] = (Words[index] << 8) + (unsigned char)message[iterations * 64 + index * 4 + j];
+                Words[index] = (Words[index] << 8) + (uint8_t)message[chunk * 64 + index * 4 + j];
+                //By casting to uint8_t(unsigned char), we ensure that the value is 1 unsigned byte
             }
         }
         Transform(Words, Hash);
@@ -129,9 +140,12 @@ void PrintHash(unsigned int Hash[8])
 
 void SHA256(string input)
 {
+    //The initial hash values are the first 32 bits of the fractional parts of the square roots of the first 8 primes
     unsigned int Hash[8] = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };
+
     Pad(input);
     Update(input, Hash);
+
     PrintHash(Hash);
 }
 
